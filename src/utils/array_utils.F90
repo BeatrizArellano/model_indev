a1b7e7dc6d08748a693d9b9a71bd1bbdd53bf8e8
Beatriz Arellano <barellano@protonmail.com>
2026-05-28 17:48:39 +0100

    Adds str_to_real_vec to convert an array of strings to real

--- src/utils/array_utils.F90
+++ src/utils/array_utils.F90
@@ -4,6 +4,7 @@ module array_utils
   private
 
   public :: reverse_array
+  public :: str_to_real_vec
 
   ! Generic interface so reverse_array works for different types
   interface reverse_array
@@ -11,7 +12,53 @@ module array_utils
      module procedure reverse_int_1d
   end interface reverse_array
 
-contains
+contains      
+
+  subroutine str_to_real_vec(strings, values, ok, errmsg, label)
+      character(*), intent(in)               :: strings(:)
+      real(rk), allocatable, intent(out)     :: values(:)
+      logical, intent(out), optional         :: ok
+      character(*), intent(out), optional    :: errmsg
+      character(*), intent(in), optional     :: label
+
+      integer :: i
+      integer :: ios
+
+      logical :: lok
+      character(len=512) :: lmsg
+
+      lok  = .true.
+      lmsg = ''
+
+      allocate(values(size(strings)))
+
+      do i = 1, size(strings)
+          read(strings(i), *, iostat=ios) values(i)
+
+          if (ios /= 0) then
+              lok = .false.
+              if (present(label)) then
+                  write(lmsg,'(A,I0,A,A,A)') &
+                      'Failed to parse value at index ', i, &
+                      ' in column "', trim(label), '".'
+              else
+                  write(lmsg,'(A,I0,A)') &
+                      'Failed to parse value at index ', i, '.'
+              end if
+              exit
+          end if
+      end do
+
+      if (present(ok)) then
+          ok = lok
+      else
+          if (.not. lok) error stop trim(lmsg)
+      end if
+      if (present(errmsg)) errmsg = trim(lmsg)
+
+  end subroutine str_to_real_vec
+
+
 
   !-------------------------------------------------------------------
   ! Reverse a real(rk) 1D array.
