89847e2562d2c350847bfe0457af8af0a2dc0d92
Beatriz Arellano <barellano@protonmail.com>
2026-05-31 18:42:50 +0100

    Adds swiflux_vars to register swi fluxes as variables for output

--- src/bio/bio_types.F90
+++ src/bio/bio_types.F90
@@ -128,6 +128,7 @@ module bio_types
       real(rk), allocatable :: diff_sed_max(:)     ! Array to store maximum effective diffusivities scaled by tortuosity for all tracers
       real(rk), allocatable :: Db_eff_solids(:)    ! Array to store effective bioturbation diffusivity (scaled by 1-phi)
       real(rk), allocatable :: swi_flux(:)         ! Flux of solutes at the sediment-water interface (tracer specific)
+      real(rk), allocatable :: swi_flux_out(:)     ! Flux of solutes at the sediment-water interface (tracer specific) This will have m d-1 units
       ! --- Runtime flags 
       logical :: use_bioturbation        = .false.
       logical :: use_bioirrigation       = .false.
@@ -206,6 +207,8 @@ module bio_types
     real(rk), allocatable :: conserved_boundary(:)             ! (n_conserved): surface+bottom totals [per m2]
     real(rk), allocatable :: conserved_total(:)                ! column-integrated totals [per m2]
     type(VarMetadata), allocatable :: conserved_vars(:)        ! metadata for conserved totals
+    ! 
+    type(VarMetadata), allocatable :: swiflux_vars(:)          ! Metadata for Sediment-water interface fluxes
     ! Other environment arrays
     type(VarMetadata), allocatable :: env_int_vars(:)          ! 1D environment variables like par/swr/pres, etc.
     ! Working arrays
